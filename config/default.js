7959ac0ec9d15cffb7fc6ecbac044826efb8a57a
Galeano Elias <galeanoeliasdavid@gmail.com>
2023-04-21 14:37:41 -0300

    FEATURE: API-USERS READY FOR THE DEPLOYED

--- config/default.js
+++ config/default.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLOUDINARY_API_KEY = exports.CLOUDINARY_API_SECRET = exports.CLOUDINARY_CLOUD_NAME = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.default = {
@@ -11,6 +10,3 @@ exports.default = {
     URI_DB: process.env.URI_DB,
     DB_NAME: process.env.DB_NAME,
 };
-exports.CLOUDINARY_CLOUD_NAME = process.env["CLOUDINARY_USER_NAME"];
-exports.CLOUDINARY_API_SECRET = process.env["CLOUDINARY_API_SECRET"];
-exports.CLOUDINARY_API_KEY = process.env["CLOUDINARY_API_KEY"];
--- config/default.ts
+++ config/default.ts
@@ -6,7 +6,3 @@ export default {
     URI_DB: <string>process.env.URI_DB,
     DB_NAME: <string>process.env.DB_NAME,
 };
-
-export const CLOUDINARY_CLOUD_NAME = <string>process.env["CLOUDINARY_USER_NAME"];
-export const CLOUDINARY_API_SECRET = <string>process.env["CLOUDINARY_API_SECRET"];
-export const CLOUDINARY_API_KEY = <string>process.env["CLOUDINARY_API_KEY"];
